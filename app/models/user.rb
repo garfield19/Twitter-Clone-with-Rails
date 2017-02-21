d66cef097de0bc47f7a0afc19e379fd34d102c6f
garfield19 <dharmie19@gmail.com>
2017-02-21 23:24:51 +0000

    added avatar uploader

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
   before_save do
     self.interest.gsub!(/[\[\]\"]/, "") if attribute_present?("interest")
   end
-  #mount_uploader :avatar, AvatarUploader
+  mount_uploader :avatar, AvatarUploader
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable, 
