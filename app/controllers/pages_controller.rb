564f90c288056045a8a24772d2cc9bb902ceecde
garfield19 <dharmie19@gmail.com>
2017-08-11 01:14:48 +0000

    sf

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -27,7 +27,8 @@ include Twitter::Autolink
     @updates = Update.all.where("user_id IN(#{following_ids}) OR
       user_id = ?",current_user.id,current_user.id).paginate(page: params[:page], per_page: 6).order(created_at: :desc)
       
-      	@interestgroups = Interestgroup.all.where('current_user.interest @> ARRAY[?]::varchar[]', ['fashion'])
+      	@interestgroups = Interestgroup.all.where(params[:current_user.interest] '@> ARRAY[?]::varchar[]', ['fashion'])
+      	
       
    # @updates = Update.paginate(page: params[:page], per_page: 6).order(created_at: :desc)
     respond_to do |format|
